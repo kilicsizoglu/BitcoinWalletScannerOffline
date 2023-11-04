39f7bffafe6c8eace62f4f0765d01846c7e5c7c8
Fatih <kilicsizoglu@gmail.com>
2023-11-04 18:12:22 +0300

    up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
+
 def main():
     rpc_user = 'user'
     rpc_password = 'password'
@@ -8,15 +9,12 @@ def main():
     rpc_url = f'http://{rpc_user}:{rpc_password}@{rpc_host}:{rpc_port}'
 
     try:
-        # Bitcoin Core RPC sunucusuna bağlan
         rpc_connection = AuthServiceProxy(rpc_url)
 
         while True:
-            # Yeni bir adres oluştur
             new_address = rpc_connection.getnewaddress()
             print(f"Yeni adres: {new_address}")
 
-            # Yeni oluşturulan adrese gönderilen toplam miktarı al
             amount_received = rpc_connection.getreceivedbyaddress(new_address)
             if amount_received <= 1:
                 print(f"Adrese henüz hiçbir miktar gönderilmedi.")
@@ -26,6 +24,7 @@ def main():
 
     except JSONRPCException as json_exception:
         print(f"RPC hatası: {json_exception.error}")
+
 
 if __name__ == '__main__':
     main()
